71a5c78fae701b0f97bbbf575aa990d5e70ce4a5
Juan Camilo Soto Soto <juaca162072@gmail.com>
2025-03-10 19:49:36 -0500

    cambios

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,21 +1,32 @@
 class AuthController {
   constructor(authService) {
-    console.log(authService)
+    console.log(authService);
     this.authService = authService;
   }
-  
-  async register(req, res) {
-      const resp = await this.authService.register(req.body).bind();
+
+  login = async (req, res) => {
+    try {
+      const resp = await this.authService.login(req.body);
       if (resp.error) {
         return res.status(resp.code).json(resp);
       }
       return res.json(resp);
-      } catch (error) {
+    } catch (error) {
       return res.status(500).json({ error: error.message });
-
     }
+  }
 
-  
+  register = async (req, res) => {
+    try {
+      const resp = await this.authService.register(req.body);
+      if (resp.error) {
+        return res.status(resp.code).json(resp);
+      }
+      return res.json(resp);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  };
 }
 
 export default AuthController;
