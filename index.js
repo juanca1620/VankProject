1b9b9b8efa8b5a62597ea6594928dede2b0ea730
Kevin Santiago <aguirre.kevin.6135@eam.edu.co>
2025-04-25 02:29:09 -0500

    Aquí agregué las librerias para el front y ando trabajandole al cliente

--- index.js
+++ index.js
@@ -1,55 +1,58 @@
-import express from 'express';
-import  { connectSequelize } from './database/Connection.js';
-import bodyParser from 'body-parser';
+import express from "express";
+import { connectSequelize } from "./database/Connection.js";
+import bodyParser from "body-parser";
 import vendedor from "./routes/Vendedor.js";
-import auth from './routes/Auth.js';
-import factura from './routes/FacturaProvedor.js';
-import productoProvedor from './routes/ProductoProvedor.js'
-import cuponProvedor from "./routes/CuponProvedor.js"
-import cuponVendedor from './routes/CuponVendedor.js';
-import productoVendedor from "./routes/ProductoVendedor.js"
-import comentario from "./routes/Comentario.js"
-import FacturaVendedor  from "./routes/FacturaVendedor.js"
-import Comentario from "./routes/Comentario.js"
-import cors from 'cors';
+import auth from "./routes/Auth.js";
+import factura from "./routes/FacturaProvedor.js";
+import productoProvedor from "./routes/ProductoProvedor.js";
+import cuponProvedor from "./routes/CuponProvedor.js";
+import cuponVendedor from "./routes/CuponVendedor.js";
+import productoVendedor from "./routes/ProductoVendedor.js";
+import comentario from "./routes/Comentario.js";
+import FacturaVendedor from "./routes/FacturaVendedor.js";
+import Comentario from "./routes/Comentario.js";
+import cors from "cors";
 
 // 2. Crear una instancia de la aplicación Express usando 'let'​
 let app = express();
 
-app.use(cors({
-  origin: '*',
-  credentials: true,
-  methods: '*',
-  allowedHeaders: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    methods: "*",
+    allowedHeaders: "*",
+  })
+);
 
 // 3. Configurar middlewares​
 app.use(bodyParser.json()); // Para parsear el cuerpo de las solicitudes en formato JSON​
-app.use(bodyParser.urlencoded({
-    extended:
-        true
-})); // Para parsear el cuerpo de las solicitudes en formato URL-encoded​
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+); // Para parsear el cuerpo de las solicitudes en formato URL-encoded​
 
-app.use('/auth', auth);
-app.use('/vendedor', vendedor);
-app.use('/facturacion',factura);
-app.use('/productoProvedor',productoProvedor)
-app.use('/cuponProvedor', cuponProvedor);
-app.use('/cuponVendedor', cuponVendedor);
-app.use('/productoVendedor', productoVendedor);
-app.use('/comentario', comentario);
-app.use('/facturaVendedor', FacturaVendedor);
+app.use("/auth", auth);
+app.use("/vendedor", vendedor);
+app.use("/facturacion", factura);
+app.use("/productoProvedor", productoProvedor);
+app.use("/cuponProvedor", cuponProvedor);
+app.use("/cuponVendedor", cuponVendedor);
+app.use("/productoVendedor", productoVendedor);
+app.use("/comentario", comentario);
+app.use("/facturaVendedor", FacturaVendedor);
 app.use("/comentario", Comentario);
 
 async function startServer() {
   try {
     await connectSequelize();
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 3001;
     app.listen(PORT, () => {
       console.log(`Servidor corriendo en http://localhost:${PORT}`);
     });
   } catch (error) {
-    console.error('Error iniciando la aplicación:', error);
+    console.error("Error iniciando la aplicación:", error);
   }
 }
 
